26958eb55ed8d805ad803f747cde52d642e38667
Altuğ Bilgin Altıntaş <altuga@gmail.com>
2026-01-17 14:50:27 +0300

    Fix account balance wiring; upgrade Spring Boot

--- src/main/java/io/reflectoring/buckpal/adapter/in/web/AccountController.java
+++ src/main/java/io/reflectoring/buckpal/adapter/in/web/AccountController.java
@@ -1,31 +1,28 @@
 package io.reflectoring.buckpal.adapter.in.web;
 
-
-
-import io.reflectoring.buckpal.application.domain.model.Account;
 import io.reflectoring.buckpal.application.domain.model.Money;
+import io.reflectoring.buckpal.application.domain.model.Account.AccountId;
 import io.reflectoring.buckpal.application.port.in.GetAccountBalanceUseCase;
-import io.reflectoring.buckpal.application.port.in.SendMoneyCommand;
+import io.reflectoring.buckpal.application.port.in.GetAccountBalanceUseCase.GetAccountBalanceQuery;
 import io.reflectoring.buckpal.common.WebAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @WebAdapter
 public class AccountController {
 
+	private final GetAccountBalanceUseCase getAccountBalanceUseCase;
 
-    public GetAccountBalanceUseCase getAccountBalanceUseCase;
-    public GetAccountBalanceUseCase.GetAccountBalanceQuery query;
+	public AccountController(GetAccountBalanceUseCase getAccountBalanceUseCase) {
+		this.getAccountBalanceUseCase = getAccountBalanceUseCase;
+	}
 
     @GetMapping(path = "/accounts/{id}/balance")
     public Money getAccountBalance(@PathVariable("id") Long accountId ) {
-
-        System.out.println(" accountId " + accountId);
-
-        return getAccountBalanceUseCase.getAccountBalance(query);
+		return getAccountBalanceUseCase.getAccountBalance(
+				new GetAccountBalanceQuery(new AccountId(accountId)));
     }
 
 
--- src/main/java/io/reflectoring/buckpal/application/domain/service/GetAccountBalanceService.java
+++ src/main/java/io/reflectoring/buckpal/application/domain/service/GetAccountBalanceService.java
@@ -3,8 +3,10 @@ package io.reflectoring.buckpal.application.domain.service;
 import io.reflectoring.buckpal.application.domain.model.Money;
 import io.reflectoring.buckpal.application.port.in.GetAccountBalanceUseCase;
 import io.reflectoring.buckpal.application.port.out.LoadAccountPort;
+import io.reflectoring.buckpal.common.UseCase;
 import java.time.LocalDateTime;
 
+@UseCase
 class GetAccountBalanceService implements GetAccountBalanceUseCase {
 
 	private final LoadAccountPort loadAccountPort;
